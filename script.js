e622795aa3c505447d301bed4d3dc85e607e3cab
Vipin Gautam <130040245+vipingautam01@users.noreply.github.com>
2023-05-15 16:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = [...players];
+const cap1 = {...person};
 
 window.players = players;
 window.person = person;
